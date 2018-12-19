816a0f2550f46a542251c649246d7da245b1b769
marvin-kose <marvinkoselnik@gmail.com>
2018-12-19 17:30:30 +0100

    FirebaseUI email and password authentication implemented.

---
+++ CoToDo/app/src/main/java/com/mobileapps/group15/cotodo/DashboardActivity.java
@@ -0,0 +1,13 @@
+/*package com.mobileapps.group15.cotodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}*/
